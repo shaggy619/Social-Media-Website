f716a89e1dbe82ff7706c3b300021c03d20f551b
shaggy <someaim1@gmail.com>
2023-09-06 18:50:53 +0545

    Added New Files

--- Social Media Website/script.js
+++ Social Media Website/script.js
@@ -1,6 +1,15 @@
 // Sidebar
 const menuItems = document.querySelectorAll(".menu-item");
 
+//Messages
+const messages = document.querySelector(".messages");
+const messagesNotifications = document.querySelector("#messages-notifications");
+const closeMessageIcon = document.querySelector(".msg-close");
+//Requests
+const frndRqst = document.querySelector("#requests");
+const request = document.querySelectorAll(".request");
+
+// ====== Sidebar =======
 const removeActiveClass = () => {
   menuItems.forEach((items) => {
     items.classList.remove("active");
@@ -15,6 +24,37 @@ menuItems.forEach((items) => {
       document.querySelector(".notification-popup").style.display = "none";
     } else {
       document.querySelector(".notification-popup").style.display = "block";
+      document.querySelector(
+        "#notifications .notification-count"
+      ).style.display = "none";
     }
+  });
+});
+
+// =========== Messeges =============
+messagesNotifications.addEventListener("click", () => {
+  messages.style.display = "block";
+  messages.style.boxShadow = "0 0 1rem var(--color-primary)";
+  setTimeout(() => {
+    messages.style.boxShadow = "none";
+  }, 2000);
+  document.querySelector(
+    "#messages-notifications .notification-count"
+  ).style.display = "none";
+});
+
+closeMessageIcon.addEventListener("click", () => {
+  messages.style.display = "none";
+});
+
+frndRqst.addEventListener("click", () => {
+  document.querySelector("#requests .notification-count").style.display =
+    "none";
+
+  request.forEach((items) => {
+    items.style.boxShadow = "0 0 1rem var(--color-primary)";
+    setTimeout(() => {
+      items.style.boxShadow = "none";
+    }, 4000);
   });
 });
